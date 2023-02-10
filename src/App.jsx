7778bdd5fdf760c72e7316622b098f7265e56d11
Samuel Velasco <samuelvelasco2698@gmail.com>
2023-02-10 16:09:05 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <form className="form">
+          <input type="text" placeholder="Avengers, StarWars, The Matrix..." />
+          <button>Buscar</button>
+        </form>
+      </header>
+      <main></main>
+    </div>
+  );
+};
+
+export default App;
